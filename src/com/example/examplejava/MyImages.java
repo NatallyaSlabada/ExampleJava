de522fda795e6c5575abc62b6e4506ca6a9275ca
Natallya Slabada <needsforme@mail.ru>
2019-09-26 16:32:22 +0300

    change pictures paths and pictures location

--- src/com/example/examplejava/MyImages.java
+++ src/com/example/examplejava/MyImages.java
@@ -3,7 +3,7 @@ package com.example.examplejava;
 import javax.swing.*;
 
 public class MyImages {
-    public static ImageIcon X = new ImageIcon("src\\x.jpg");
-    public static ImageIcon O = new ImageIcon("src\\o.jpg");
-    public static ImageIcon D = new ImageIcon("src\\d.jpg");
+    public static ImageIcon X = new ImageIcon("x.jpg");
+    public static ImageIcon O = new ImageIcon("o.jpg");
+    public static ImageIcon D = new ImageIcon("d.jpg");
 }
